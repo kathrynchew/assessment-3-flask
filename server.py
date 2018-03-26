ce960baaa441c9680f3bdc67336e5a1f3030ae76
kathryn <kathrynchew@gmail.com>
2018-03-26 04:56:49 +0000

    Name collected & added to session; jinja formatted to add name dynamically; added redirect & flash msg if no name given

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request, render_template, session
+from flask import Flask, redirect, request, render_template, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
 
@@ -37,11 +37,29 @@ MOST_LOVED_MELONS = {
 # YOUR ROUTES GO HERE
 
 
+@app.route("/")
+def get_start_info():
+    """ Renders home page, gets user's name to populate elsewhere on site """
+
+    return render_template("homepage.html")
+
+@app.route("/get-name")
+def render_name():
+    """ Print username """
+    name = request.args.get("name")
+    session["name"] = name.title()
+    return redirect("/top-melons")
+
+
 @app.route("/top-melons")
 def show_top_melons():
     """ Renders list of most beloved melons """
 
-    return render_template("top-melons.html", loved_melons=MOST_LOVED_MELONS)
+    if "name" in session:
+        return render_template("top-melons.html", loved_melons=MOST_LOVED_MELONS)
+    else:
+        flash("You haven't entered your name yet! Please try again.")
+        return redirect("/")
 
 
 if __name__ == "__main__":
@@ -51,5 +69,6 @@ if __name__ == "__main__":
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
     app.run(host="0.0.0.0")
